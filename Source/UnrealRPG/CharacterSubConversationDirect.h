c41f9814f1fa8f381fd0d51c27fa7bc9c46bbd58
Chuck <scottwood293@gmail.com>
2017-09-09 17:52:23 -0500

    Fixed lighting at night, modified the conversation system to allow sub conversations for ally characters. Created Data asset class for ally character to store conversation directories for sub conversations

---
+++ Source/UnrealRPG/CharacterSubConversationDirect.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Engine/DataTable.h"
+#include "CharacterSubConversationDirect.generated.h"
+
+USTRUCT()
+struct FSubConversationMap
+{
+	GENERATED_USTRUCT_BODY()
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TMap<int, UDataTable*> SubConversations;
+};
+
+UCLASS(BlueprintType)
+class UNREALRPG_API UCharacterSubConversationDirect : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TMap<UDataTable*, FSubConversationMap> SubConversationDirectory;
+};
